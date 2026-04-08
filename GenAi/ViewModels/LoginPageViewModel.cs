e21a981bcc4ff829f7951218d8e835ce3c93a34a
Shivam Mishra <shivamtestapp76@gmail.com>
2026-04-09 00:38:48 +0530

    login page and view model

---
+++ GenAi/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenAi.ViewModels
+{
+    internal class LoginPageViewModel
+    {
+    }
+}
---
+++ GenAi/Views/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace GenAi.Views;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
